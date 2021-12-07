64a43a2999196c7d458b1cc72d8c39513ebb079a
Chris Young <c.young@callaghaninnovation.govt.nz>
2021-12-08 12:21:05 +1300

    Update VerticalFederalForm.cs
    Made XML config files unique outputs for each measurement.

--- Vertical Federal App/VerticalFederalForm.cs
+++ Vertical Federal App/VerticalFederalForm.cs
@@ -43,8 +43,8 @@ namespace Vertical_Federal_App
             tdr = new TemperatureRetrieved(TemperatureReceived);
             pgr = new PrintGaugeResultsToRichTextbox(PrintGaugeResults);
             Measurement.working_gauge = new GaugeBlock(true);
-            INI2XML.DoIni2XmlConversion(ref messagesRichTextBox, @"G:\Shared drives\MSL - Length\Length\EQUIPREG\XML Files\cal_data.xml", @"G:\Shared drives\MSL - Length\Length\EQUIPREG\cal_data.ini",false);
-            INI2XML.DoIni2XmlConversion(ref messagesRichTextBox, @"G:\Shared drives\MSL - Length\Length\Technical Procedures\XML Files\config_uncertainty.xml", @"G:\Shared drives\MSL - Length\Length\Technical Procedures\config_uncertainty.ini", true);
+            INI2XML.DoIni2XmlConversion(ref messagesRichTextBox, @"G:\Shared drives\MSL - Length\Length\EQUIPREG\XML Files\cal_data_federal_measurement"+System.DateTime.Now.Ticks.ToString()+".xml", @"G:\Shared drives\MSL - Length\Length\EQUIPREG\cal_data.ini",false);
+            INI2XML.DoIni2XmlConversion(ref messagesRichTextBox, @"G:\Shared drives\MSL - Length\Length\Technical Procedures\XML Files\config_uncertainty_federal_measurement"+System.DateTime.Now.Ticks.ToString()+".xml", @"G:\Shared drives\MSL - Length\Length\Technical Procedures\config_uncertainty.ini", true);
             if (INI2XML.Converted) INI2XML.PopulateReferenceGaugeComboBox(ref referenceSetComboBox, true);  //initially assume metric reference gauges (second argument true).
             Measurement.calibration_gauge_sets = new List<GaugeBlockSet>();  //make a new list for calibration gauge sets
             Measurement.reference_gauge_sets = new List<GaugeBlockSet>();
