f382721ef479f3489778fef78d2be7e9fe1751a3
Chris Young <c.young@callaghaninnovation.govt.nz>
2023-02-28 15:58:17 +1300

    Update Report.Designer.cs
    Updates to GUI for latex report generation

--- Vertical Federal App/Report.Designer.cs
+++ Vertical Federal App/Report.Designer.cs
@@ -45,47 +45,54 @@ namespace Vertical_Federal_App
             this.imageFileRichTextBox = new System.Windows.Forms.RichTextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.ImageFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.sn_note = new System.Windows.Forms.Label();
+            this.serial_Number_Note = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // PhysicalAddress
             // 
-            this.PhysicalAddress.Location = new System.Drawing.Point(69, 321);
+            this.PhysicalAddress.Location = new System.Drawing.Point(46, 429);
+            this.PhysicalAddress.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PhysicalAddress.Name = "PhysicalAddress";
-            this.PhysicalAddress.Size = new System.Drawing.Size(481, 26);
+            this.PhysicalAddress.Size = new System.Drawing.Size(322, 20);
             this.PhysicalAddress.TabIndex = 1;
             this.PhysicalAddress.TextChanged += new System.EventHandler(this.PhysicalAddress_TextChanged);
             // 
             // BussinessName
             // 
-            this.BussinessName.Location = new System.Drawing.Point(69, 255);
+            this.BussinessName.Location = new System.Drawing.Point(47, 381);
+            this.BussinessName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BussinessName.Name = "BussinessName";
-            this.BussinessName.Size = new System.Drawing.Size(481, 26);
+            this.BussinessName.Size = new System.Drawing.Size(322, 20);
             this.BussinessName.TabIndex = 2;
             this.BussinessName.TextChanged += new System.EventHandler(this.BussinessName_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(65, 232);
+            this.label2.Location = new System.Drawing.Point(44, 366);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(297, 20);
+            this.label2.Size = new System.Drawing.Size(198, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "NZ Registered Bussiness Name of Client";
             // 
             // AddressLabel
             // 
             this.AddressLabel.AutoSize = true;
-            this.AddressLabel.Location = new System.Drawing.Point(65, 298);
+            this.AddressLabel.Location = new System.Drawing.Point(44, 414);
+            this.AddressLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.AddressLabel.Name = "AddressLabel";
-            this.AddressLabel.Size = new System.Drawing.Size(129, 20);
+            this.AddressLabel.Size = new System.Drawing.Size(87, 13);
             this.AddressLabel.TabIndex = 5;
             this.AddressLabel.Text = "Physical Address";
             // 
             // WriteReportButton
             // 
-            this.WriteReportButton.Location = new System.Drawing.Point(69, 502);
+            this.WriteReportButton.Location = new System.Drawing.Point(47, 464);
+            this.WriteReportButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.WriteReportButton.Name = "WriteReportButton";
-            this.WriteReportButton.Size = new System.Drawing.Size(481, 71);
+            this.WriteReportButton.Size = new System.Drawing.Size(321, 46);
             this.WriteReportButton.TabIndex = 6;
             this.WriteReportButton.Text = "Write Report to File";
             this.WriteReportButton.UseVisualStyleBackColor = true;
@@ -93,9 +100,10 @@ namespace Vertical_Federal_App
             // 
             // JobNoText
             // 
-            this.JobNoText.Location = new System.Drawing.Point(69, 61);
+            this.JobNoText.Location = new System.Drawing.Point(46, 40);
+            this.JobNoText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.JobNoText.Name = "JobNoText";
-            this.JobNoText.Size = new System.Drawing.Size(481, 26);
+            this.JobNoText.Size = new System.Drawing.Size(322, 20);
             this.JobNoText.TabIndex = 8;
             this.JobNoText.Text = "LXXXX/JXXXXX";
             this.JobNoText.TextChanged += new System.EventHandler(this.JobNoText_TextChanged);
@@ -103,25 +111,28 @@ namespace Vertical_Federal_App
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(65, 38);
+            this.label1.Location = new System.Drawing.Point(43, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(168, 20);
+            this.label1.Size = new System.Drawing.Size(115, 13);
             this.label1.TabIndex = 9;
             this.label1.Text = "L Number/Job Number";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(65, 93);
+            this.label3.Location = new System.Drawing.Point(43, 60);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 10;
             // 
             // ReportNumberTextbox
             // 
-            this.ReportNumberTextbox.Location = new System.Drawing.Point(69, 128);
+            this.ReportNumberTextbox.Location = new System.Drawing.Point(46, 88);
+            this.ReportNumberTextbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ReportNumberTextbox.Name = "ReportNumberTextbox";
-            this.ReportNumberTextbox.Size = new System.Drawing.Size(481, 26);
+            this.ReportNumberTextbox.Size = new System.Drawing.Size(322, 20);
             this.ReportNumberTextbox.TabIndex = 11;
             this.ReportNumberTextbox.Text = "XXXX";
             this.ReportNumberTextbox.TextChanged += new System.EventHandler(this.ReportNumberTextbox_TextChanged);
@@ -129,17 +140,19 @@ namespace Vertical_Federal_App
             // Report_Number_label
             // 
             this.Report_Number_label.AutoSize = true;
-            this.Report_Number_label.Location = new System.Drawing.Point(65, 105);
+            this.Report_Number_label.Location = new System.Drawing.Point(43, 73);
+            this.Report_Number_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Report_Number_label.Name = "Report_Number_label";
-            this.Report_Number_label.Size = new System.Drawing.Size(118, 20);
+            this.Report_Number_label.Size = new System.Drawing.Size(79, 13);
             this.Report_Number_label.TabIndex = 12;
             this.Report_Number_label.Text = "Report Number";
             // 
             // ManufacturerTextbox
             // 
-            this.ManufacturerTextbox.Location = new System.Drawing.Point(69, 191);
+            this.ManufacturerTextbox.Location = new System.Drawing.Point(45, 134);
+            this.ManufacturerTextbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ManufacturerTextbox.Name = "ManufacturerTextbox";
-            this.ManufacturerTextbox.Size = new System.Drawing.Size(481, 26);
+            this.ManufacturerTextbox.Size = new System.Drawing.Size(322, 20);
             this.ManufacturerTextbox.TabIndex = 13;
             this.ManufacturerTextbox.Text = "Enter the manufacturer\'s name";
             this.ManufacturerTextbox.TextChanged += new System.EventHandler(this.ManufacturerTextbox_TextChanged);
@@ -147,17 +160,19 @@ namespace Vertical_Federal_App
             // ManufacturerLabel
             // 
             this.ManufacturerLabel.AutoSize = true;
-            this.ManufacturerLabel.Location = new System.Drawing.Point(65, 168);
+            this.ManufacturerLabel.Location = new System.Drawing.Point(43, 119);
+            this.ManufacturerLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ManufacturerLabel.Name = "ManufacturerLabel";
-            this.ManufacturerLabel.Size = new System.Drawing.Size(104, 20);
+            this.ManufacturerLabel.Size = new System.Drawing.Size(70, 13);
             this.ManufacturerLabel.TabIndex = 14;
             this.ManufacturerLabel.Text = "Manufacturer";
             // 
             // addImageButton
             // 
-            this.addImageButton.Location = new System.Drawing.Point(607, 502);
+            this.addImageButton.Location = new System.Drawing.Point(405, 464);
+            this.addImageButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.addImageButton.Name = "addImageButton";
-            this.addImageButton.Size = new System.Drawing.Size(456, 71);
+            this.addImageButton.Size = new System.Drawing.Size(304, 46);
             this.addImageButton.TabIndex = 15;
             this.addImageButton.Text = "Add Image File";
             this.addImageButton.UseVisualStyleBackColor = true;
@@ -165,9 +180,10 @@ namespace Vertical_Federal_App
             // 
             // imageFileRichTextBox
             // 
-            this.imageFileRichTextBox.Location = new System.Drawing.Point(607, 61);
+            this.imageFileRichTextBox.Location = new System.Drawing.Point(405, 40);
+            this.imageFileRichTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.imageFileRichTextBox.Name = "imageFileRichTextBox";
-            this.imageFileRichTextBox.Size = new System.Drawing.Size(456, 415);
+            this.imageFileRichTextBox.Size = new System.Drawing.Size(305, 409);
             this.imageFileRichTextBox.TabIndex = 16;
             this.imageFileRichTextBox.Text = "";
             // 
@@ -175,9 +191,10 @@ namespace Vertical_Federal_App
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(603, 38);
+            this.label4.Location = new System.Drawing.Point(402, 25);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(624, 30);
+            this.label4.Size = new System.Drawing.Size(276, 13);
             this.label4.TabIndex = 17;
             this.label4.Text = "Image File List (you can add multiple images to the report)";
             // 
@@ -185,11 +202,32 @@ namespace Vertical_Federal_App
             // 
             this.ImageFileDialog.FileName = "ImageFileDialog";
             // 
+            // sn_note
+            // 
+            this.sn_note.AutoSize = true;
+            this.sn_note.Location = new System.Drawing.Point(44, 244);
+            this.sn_note.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.sn_note.Name = "sn_note";
+            this.sn_note.Size = new System.Drawing.Size(99, 13);
+            this.sn_note.TabIndex = 18;
+            this.sn_note.Text = "Serial Number Note";
+            // 
+            // serial_Number_Note
+            // 
+            this.serial_Number_Note.Location = new System.Drawing.Point(46, 260);
+            this.serial_Number_Note.Name = "serial_Number_Note";
+            this.serial_Number_Note.Size = new System.Drawing.Size(321, 92);
+            this.serial_Number_Note.TabIndex = 19;
+            this.serial_Number_Note.Text = "The gauge block set has the serial number XXXX.   Each gauge block is marked with" +
+    " a nominal size and a unique serial number.";
+            // 
             // Report
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1125, 611);
+            this.ClientSize = new System.Drawing.Size(750, 544);
+            this.Controls.Add(this.serial_Number_Note);
+            this.Controls.Add(this.sn_note);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.imageFileRichTextBox);
             this.Controls.Add(this.addImageButton);
@@ -205,6 +243,7 @@ namespace Vertical_Federal_App
             this.Controls.Add(this.label2);
             this.Controls.Add(this.BussinessName);
             this.Controls.Add(this.PhysicalAddress);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Report";
             this.Text = "Report";
             this.ResumeLayout(false);
@@ -229,5 +268,7 @@ namespace Vertical_Federal_App
         private System.Windows.Forms.RichTextBox imageFileRichTextBox;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.OpenFileDialog ImageFileDialog;
+        private System.Windows.Forms.Label sn_note;
+        private System.Windows.Forms.RichTextBox serial_Number_Note;
     }
 }
